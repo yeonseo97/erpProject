0fb22959bb207ef030ec5e947aa553f2f7fad94c
yeonseo <dustj0824@naver.com>
2025-09-18 22:26:23 +0900

    DB 연결 및 초기테스트

---
+++ src/main/java/com/yeon/erpproject/ErpProjectApplication.java
@@ -0,0 +1,13 @@
+package com.yeon.erpproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErpProjectApplication.class, args);
+    }
+
+}
